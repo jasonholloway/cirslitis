3f92e308192000cbaf115043eea781ccf43ca98e
Jason Holloway <miceeatnicerice@hotmail.com>
2019-07-04 14:23:40 +0100

    nicely structured

--- build.sh
+++ build.sh
@@ -1,12 +1,43 @@
 #!/bin/bash
 
 set -x
+set -e
 
-source .env
+name=cirslitis
+commit=$(git rev-parse --short HEAD)
 
-mkdir -p build && cd build
+main() {
+	loadEnv
+	clean
+	build
+	copyOutput
+}
 
-(envsubst < ../haproxy.cfg) > haproxy.cfg
+loadEnv() {
+	[[ -f ".env" ]] && source .env;
+}
 
-linuxkit build -format vhd ../cirslitis.yml
+clean() {
+	rm -rf build
+}
 
+build() {
+	cp -R src build
+	(envsubst < src/haproxy.cfg) > build/haproxy.cfg
+
+	(cd build;
+		linuxkit build -format raw-efi $name.yml;
+		qemu-img convert \
+						-f raw \
+						-O vpc -o subformat=fixed,force_size \
+						$name-efi.img \
+						$name.vhd)
+}
+
+copyOutput() {
+	mkdir -p out
+	mv build/$name.vhd out/$name-$commit.vhd
+	clean
+}
+
+main
