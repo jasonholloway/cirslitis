442783edd5bc774fc6e1f51893d8bfe756202d08
Jason Holloway <miceeatnicerice@hotmail.com>
2019-07-07 20:29:56 +0000

    removed it from docker run

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ builder=tmp$(uuidgen)
  
 docker build -t "$builder" . 1>&2
 
-docker run --rm -it \
+docker run --rm \
 			 --privileged \
 			 -v /var/run/docker.sock:/var/run/docker.sock \
 			 -v $PWD/.env:/.env \
