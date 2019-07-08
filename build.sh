19ef3b642558b418785a09a1a6a257a9893864ea
Jason Holloway <miceeatnicerice@hotmail.com>
2019-07-08 00:07:18 +0000

    small image loads on azure again

--- build.sh
+++ build.sh
@@ -8,9 +8,8 @@ builder=tmp$(uuidgen)
 docker build -t "$builder" . 1>&2
 
 docker run --rm \
-			 --privileged \
 			 -v /var/run/docker.sock:/var/run/docker.sock \
-			 -v $PWD/.env:/.env \
+			 -v $PWD/.env:/build/.env \
 			 "$builder" \
 			 "$@"
 
--- buildImage.sh
+++ buildImage.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-set -x
 set -e
 
 name=cirslitis
@@ -23,18 +22,18 @@ prepFile() {
 }
 
 buildVhd() {
-	linuxkit build \
-					-format raw-bios \
-					-disable-content-trust \
-					"${name}.yml" \
-					1>&2
+ 	linuxkit build \
+ 					-format raw-bios \
+ 					-disable-content-trust \
+ 					"${name}.yml" \
+ 					1>&2
 
-	qemu-img convert \
-					-O vpc \
-					-o subformat=fixed,force_size \
-					"${name}-bios.img" \
-					"${name}.vhd" \
-					1>&2
+ 	qemu-img convert \
+ 					-O vpc \
+ 					-o subformat=fixed,force_size \
+ 					"${name}-bios.img" \
+ 					"${name}.vhd" \
+ 					1>&2
 }
 
 pipeOut() {
